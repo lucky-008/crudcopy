a0edb352be8f61c5db223f93d540a554ca52cfca
lucky <luckyverma14082004@gmail.com>
2026-01-08 15:32:01 +0530

    modal openclose

--- client/vite-project/src/App.jsx
+++ client/vite-project/src/App.jsx
@@ -6,6 +6,7 @@ import UserTable from "./components/UserTable";
 import UserModel from "./components/UserModel";
 import { getUsers, searchUsers,getStats,addUser,deleteUser } from "./api/userApi";
 import { useState } from "react";
+import { useEffect } from "react";
  
 
 function App() {
@@ -17,7 +18,7 @@ function App() {
   const [isModalOpen, setIsModalOpen]= useState(false)
   const [formData, setFormData]= useState({
     name:"",
-    email:"",
+    email:"", 
     phone:"",
     status:"active",
   })
@@ -28,28 +29,117 @@ const [itemsPerpage, setItemsPerPage] = useState(5);
 const [totalPages, setTotalPages] = useState(0);
 
 const status = ["Active", "Inactive"];
+
+useEffect(()=>{
+  fetchUsers();
+
+},[currentPage,itemsPerpage])
+
+
+useEffect(()=>{
+  if(searchTerm) handleSearch();
+  else fetchUsers();
+
+},[searchTerm])
+const fetchStats = async () => {
+  const data = await getStats();
+  setStats(data);
+};
+
+const fetchUsers = async () => {
+  const data = await getUsers(currentPage, itemsPerpage);
+  setUsers(data.users);
+  setTotalPages(data.totalPages);
+  setTotalUsers(data.totalUsers);
+  fetchStats();
+};
+
+const handleSearch = async () => {
+  const data = await searchUsers(searchTerm, currentPage, itemsPerpage);
+  setUsers(data.users);
+  setTotalPages(data.totalPages);
+  setTotalUsers(data.totalUsers);
+};
+const handleSubmit = async () => {
+  if (!formData.name || !formData.email || !formData.phone)
+    return alert("Fill all fields");
+
+  setLoading(true);
+
+  try {
+    if (editingItem)
+      await updateUser(editingItem._id, formData);
+    else
+      await addUser(formData);
+
+    fetchUsers();
+    closeModel();
+  } catch (error) {
+    alert(error.message);
+  }
+
+  setLoading(false);
+};
+
+const handleDelete = async (id) => {
+  if (window.confirm("Are you sure")) {
+    await deleteUser(id);
+    fetchUsers();
+  }
+};
+const openModel = (item = null) => {
+  if (item) {
+    setEditingItem(item);
+    setFormData(item);
+  } else {
+    setEditingItem(null);
+    setFormData({ name: "", email: "", phone: "", status: "Active" });
+  }
+  setIsModalOpen(true);
+};
+
+const closeModel = () => {
+  setIsModalOpen(false);
+  setEditingItem(null);
+  setFormData({ name: "", email: "", phone: "", status: "Active" });
+};
+
+
+
+
+
+
+
   return (
     <div className="min-h-screen bg-gray-950">
       {/* Header */}
       <header className="bg-gray-900 shadow-xl border-b border-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex justify-between items-center">
-          <div className="flex items-center gap-3">
-            <div className="p-2 bg-green-500 rounded-lg">
-              <Users size={28} className="text-gray-900" />
+  <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-6 flex justify-between items-center">
 
-            </div>
-            <div>
-              <h1 className="text-3l font-bold text-white" >User Management </h1>
-              <p className="text-gray-400 mt-1">MERN stack application</p>
+    {/* LEFT SIDE */}
+    <div className="flex items-center gap-3">
+      <div className="p-2 bg-green-500 rounded-lg">
+        <Users size={28} className="text-gray-900" />
+      </div>
 
-            </div>
-            <button className="flex items-center gap-2 bg-amber-500 text-gray-900 px-5 py-2.5 rounded-lg *
-             hover:bg-green-400 transition-colors shadow-lg font-semibold">
-              <Plus size={20} />Add user
-             </button>
-          </div>
-        </div>
-      </header>
+      <div>
+        <h1 className="text-xl font-bold text-white">User Management</h1>
+        <p className="text-gray-400 mt-1">MERN stack application</p>
+      </div>
+    </div>
+
+    {/* RIGHT SIDE (ADD USER BUTTON) */}
+    <button
+      onClick={() => openModel()}
+      className="flex items-center gap-2 bg-green-500 text-gray-900 px-5 py-2.5 rounded-lg hover:bg-amber-400 transition-colors shadow-lg font-semibold"
+    >
+      <Plus size={20} />
+      Add user
+    </button>
+
+  </div>
+</header>
+
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
        <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mb-8">
@@ -62,7 +152,11 @@ const status = ["Active", "Inactive"];
 
 <UserTable />
 
-{/* <UserModel /> */}
+<UserModel
+  isOpen={isModalOpen}
+  onClose={() => setIsModalOpen(false)}
+/>
+
 
 
       </main>
--- client/vite-project/src/components/UserModel.jsx
+++ client/vite-project/src/components/UserModel.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Check, X } from "lucide-react";
 
-function UserModel() {
+function UserModel({isOpen,onClose}) {
+if(!isOpen) return null;
+
   return (
     <div className="fixed inset-0 bg-black/80 flex items-center justify-center p-4 z-50">
       <div className="bg-gray-900 rounded-lg shadow-2xl max-w-2xl w-full max-h-screen overflow-y-auto border border-gray-800">
@@ -9,7 +11,7 @@ function UserModel() {
         {/* Header */}
         <div className="flex justify-between items-center p-6 border-b border-gray-800">
           <h2 className="text-2xl font-bold text-white">Add New User</h2>
-          <button className="text-gray-400 hover:text-white transition-all">
+          <button className="text-gray-400 hover:text-white transition-all"onClick={onClose} >
             <X size={24} />
           </button>
         </div>
@@ -64,7 +66,7 @@ function UserModel() {
 
 
 
-<div className="flex gap-3 mt-6">
+<div className="flex gap-3 mt-6 ">
   <button
     type="button"
     className="flex-1 px-4 py-2.5 bg-gray-800 border border-gray-700 text-gray-300 rounded-lg hover:bg-gray-700 transition-all"
