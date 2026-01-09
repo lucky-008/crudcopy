129f7552a1ac64f0bfae4c161a468349bd4b2e34
lucky <luckyverma14082004@gmail.com>
2026-01-09 11:45:25 +0530

    searchbar updated

--- client/vite-project/src/components/SearchBar.jsx
+++ client/vite-project/src/components/SearchBar.jsx
@@ -10,9 +10,9 @@ function SearchBar({ value, onChange, onClear, itemsPerPage, onitemsPerPageChang
         onChange={(e) => onChange(e.target.value)}
         className="px-4 py-2 rounded-lg bg-gray-800 text-white border border-gray-700 outline-none"
       />
-      <button onClick={onClear} className="px-3 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 text-white">
+      {/* <button onClick={onClear} className="px-3 py-2 bg-gray-700 rounded-lg hover:bg-gray-600 text-white">
         Clear
-      </button>
+      </button> */}
       <select value={itemsPerPage} onChange={(e) => onitemsPerPageChange(Number(e.target.value))} className="px-3 py-2 bg-gray-800 text-white rounded-lg">
         {[5, 10, 20, 50].map((n) => <option key={n} value={n}>{n} per page</option>)}
       </select>
